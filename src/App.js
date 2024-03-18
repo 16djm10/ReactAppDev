03cbbfaac10f6cdc93f1b73fc0095bf0ec16b148
David Morton <davidmorton_@outlook.com>
2024-03-18 16:35:05 -0400

    installed react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import './index.css'; // importing the css file
 import Employee from './components/Employee';// importing components
 import { useState } from 'react'; // import the useState hook
+import { v4 as uuidv4 } from 'uuid'; // import the uuid/guid package
 
 function App() {
   const [role, setRole] = useState('dev'); // dev is just default value
   const showEmployees = true;
-  return ( // this returns the JSX that will be rendered to the DOM
-    // this is the root element
-    <div className="bg-red-300">   
+  const [employees, setEmployees] = useState([
+      {
+        name: "David", 
+        role: "R&D Engineer", 
+        img: "https://images.pexels.com/photos/3777943/pexels-photo-3777943.jpeg"
+      },
+      {
+        name: "Caleb", 
+        role: "Developer", 
+        img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+      },
+      {
+        name: "Jane", 
+        role: "Manager", 
+        img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
+      },
+      {
+        name: "Abby",
+        role: "HR",
+        img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
+      },
+      {
+        name: "Josh",
+        role: "IT Support",
+        img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+      }
+    ]); // setting the default value of the employees state
+  return (
+    <div className="App">   
         {showEmployees ? 
             <>
               <input 
@@ -17,9 +44,15 @@ function App() {
                   setRole(e.target.value);
                 }}
               />
-              <Employee name="David" role="R&D Engineer" />
-              <Employee name="Abby" role="Software Developer" />
-              <Employee  name="Jane" role={role} />
+              <div className="flex flex-wrap justify-center">
+                {/* map allows us to loop through the employees array */}
+                {employees.map((employee) => {
+                  return (
+                    <Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img} alt={employee.name}
+                    />
+                  );
+                })}
+              </div>
             </>
           :
             <h3>No access allowed</h3>
